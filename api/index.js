2989b803f9544f620ad7b3adf9ddd25a18f3e7d8
DeveloperZER0 <w-piwowar@wp.pl>
2022-10-11 20:16:41 +0200

    cors

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const express = require('express')
+var cors = require('cors')
 
 const app = express() //express
 
-app.use(express.json())
+app.use(cors())
 
 app.get('/', async (req, res) => {
     const allMarkers = await prisma.post.findMany()
